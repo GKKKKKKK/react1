3676bb4b90a66e75398c128bdf8fc220650c3ec1
27bslash <greenpear68@yahoo.co.uk>
2025-10-10 11:59:33 +0100

    shortened and capitalized table headers

--- src/components/table/tableHeader/tableHeader.jsx
+++ src/components/table/tableHeader/tableHeader.jsx
@@ -1,22 +1,26 @@
+import { Typography } from "@mui/material";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
 const TableHeader = ({ defaultColumns }) => {
   const columnWidths = {
-    "Insight name": { xs: "120px", md: "200px" },
-    "Technology platform": { xs: "120px", md: "150px" },
-    "Description of technology": { xs: "250px", md: "400px" }, // wider on desktop
-    "Anticipated TRL": { xs: "60px", md: "120px" },
-    "Year of entry into the database": { xs: "120px", md: "150px" },
-    "Tag 1": { xs: "80px", md: "100px" },
-    "Tag 2": { xs: "80px", md: "100px" },
-    "Tag 3": { xs: "80px", md: "100px" },
-    "Details": { xs: "60px", md: "80px" },
+    // "Insight name": { xs: "120px", md: "200px" },
+    // "Technology platform": { xs: "120px", md: "150px" },
+    // "Description of technology": { xs: "250px", md: "400px" }, // wider on desktop
+    // "Anticipated TRL": { xs: "60px", md: "120px" },
+    // "Year of entry into the database": { xs: "120px", md: "150px" },
+    // "Tag 1": { xs: "80px", md: "100px" },
+    // "Tag 2": { xs: "80px", md: "100px" },
+    // "Tag 3": { xs: "80px", md: "100px" },
+    // Details: { xs: "60px", md: "80px" },
   };
 
   const displayNames = {
     "Year of entry into the database": "Year of entry", // shorter label
+    "Technology platform": "technology",
+    "Insight name": "insight",
+    "Description of technology": "description",
   };
 
   return (
@@ -32,7 +36,9 @@ const TableHeader = ({ defaultColumns }) => {
               overflow: "visible",
             }}
           >
-            {displayNames[col] || col}
+            <Typography textTransform={"capitalize"} >
+              {displayNames[col] || col}
+            </Typography>
           </TableCell>
         ))}
 
@@ -40,7 +46,7 @@ const TableHeader = ({ defaultColumns }) => {
         <TableCell
           key="Details"
           sx={{
-            width: columnWidths["Details"].md,
+            // width: columnWidths["Details"].md,
             textAlign: "center",
             fontWeight: "bold",
           }}
