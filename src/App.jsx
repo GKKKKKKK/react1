183585fc100f9018f9644dffb8e816a261fa3234
27bslash <greenpear68@yahoo.co.uk>
2025-10-10 12:39:27 +0100

    removed padding on root

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Index from "./components/table/mainPage";
 import MainBanner from "./components/banner/MainBanner";
 import AboutDB from "./components/aboutDB/aboutDB";
 
-
-
 //export const apiUrl = "http://localhost:3001";
-export const dataApiUrl = "https://57zzacs2v9.execute-api.eu-west-2.amazonaws.com";
-export const imageApiUrl = "https://y6h7ldgm6j.execute-api.eu-west-2.amazonaws.com";
+export const dataApiUrl =
+  "https://57zzacs2v9.execute-api.eu-west-2.amazonaws.com";
+export const imageApiUrl =
+  "https://y6h7ldgm6j.execute-api.eu-west-2.amazonaws.com";
 
 function App() {
   return (
@@ -21,14 +21,15 @@ function App() {
             sx={{
               display: "flex",
               flexDirection: "column",
-              alignItems: "stretch",  // let children fill width
+              alignItems: "stretch", // let children fill width
               gap: 3,
               mb: 4,
               width: "100%",
-          //  maxWidth: { xs: "98%", sm: "95%", md: "90%", lg: "80%" },
+              //  maxWidth: { xs: "98%", sm: "95%", md: "90%", lg: "80%" },
               mx: "auto",
               px: { xs: 1, sm: 2, md: 4 },
               boxSizing: "border-box",
+              padding: "0px !important",
             }}
           >
             <MainBanner />
@@ -39,9 +40,7 @@ function App() {
                 width: "100%",
               }}
             >
-
               <AboutDB />
-              
             </Box>
             <Index />
           </Box>
